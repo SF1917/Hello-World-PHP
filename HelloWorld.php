898e7484903e75a7324a798f31cf3e58e160c093
SF1917 <73768888+SF1917@users.noreply.github.com>
2021-03-03 10:06:19 +0330

    Main Files
    Main App Files

---
+++ HelloWorld.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>My First PHP Application</title>
+    </head>
+    <body>
+        <?php
+        echo 'Hello World!';
+        echo '<br/>';
+        echo 'Welcome to PHP Programming Course.'
+        ?>
+    </body>
+</html>
